67c1bfa57f16b773bf7a3a999a67cef84c3ebad0
Lamin Kamara <tropax6@gmail.com>
2018-03-05 11:09:45 +0100

    Views started

---
+++ Plejehjem Opgave CSharp/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Plejehjem_Opgave_CSharp.Models;
+
+namespace Plejehjem_Opgave_CSharp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            //make validation
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            // use database to validate
+            return View();
+        }
+
+
+        public ActionResult LoggedIn()
+        {
+            if (Session["UserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ Plejehjem Opgave CSharp/Models/UserAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Plejehjem_Opgave_CSharp.Models
+{
+    public class UserAccount
+    {
+
+        [Key]
+        public int UserID { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Firstname is required.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Lastname is required.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email ID")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-mm-yyyy}")]
+        public DateTime DateOfBirth { get; set; } 
+
+        [Display(Name = "Username is required")]
+        public string UserName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Minimum 6 characters required")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password do not match please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+    }
+}

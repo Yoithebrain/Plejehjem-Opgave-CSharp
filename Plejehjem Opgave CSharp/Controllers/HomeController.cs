736d89a7a80b3155141b0a85e59ab6e70d02df85
Thomas <holmegaard1996@gmail.com>
2018-03-06 12:47:33 +0100

    added google maps API intergraions & rute plan view

--- Plejehjem Opgave CSharp/Controllers/HomeController.cs
+++ Plejehjem Opgave CSharp/Controllers/HomeController.cs
@@ -3,11 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Plejehjem_Opgave_CSharp.Models;
 
 namespace Plejehjem_Opgave_CSharp.Controllers
 {
     public class HomeController : Controller
     {
+        private List<CitizensInformation> citizens;
+
+        public HomeController()
+        {
+
+            citizens = new List<CitizensInformation>()
+            {
+                //following is dummy data. 
+                //TODO: This is to be replaced with data from Database
+                new CitizensInformation()
+                {
+                    address = "Lygten 37",
+                    citizensName = "Ole Kristiansen",
+                    CPRNumber = "165453452",
+                    visitingTime = "12/12-2017 09:22"
+                },
+                new CitizensInformation()
+                {
+                    address = "Brandstrupvej 4",
+                    citizensName = "Kurt jensen",
+                    CPRNumber = "2345234513",
+                    visitingTime = "12/12-2017 10:30"
+                },
+
+                new CitizensInformation()
+                {
+                    address = "William boothsvej 17",
+                    citizensName = "Hans Andersen",
+                    CPRNumber = "463456345",
+                    visitingTime = "12/12-2017 11:42"
+                }
+               
+
+            };
+        }
+
+        public ActionResult RutePlan()
+        {
+
+            return View(citizens);
+
+
+        }
 
         public ActionResult Details_VP()
         {
---
+++ Plejehjem Opgave CSharp/Models/CitizensInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plejehjem_Opgave_CSharp.Models
+{
+    public class CitizensInformation
+    {
+        public string CPRNumber { get; set; }
+        public string citizensName { get; set; }
+        public string address { get; set; }
+        public string visitingTime { get; set; }
+
+    }
+}

27b3659402ab65bfef03f6bee37e6c3dc52f65b3
Thomas <holmegaard1996@gmail.com>
2018-03-18 21:49:34 +0100

    added create citizens view

---
+++ Plejehjem Opgave CSharp/Models/FullCitizensInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plejehjem_Opgave_CSharp.Models
+{
+    public class FullCitizensInfo
+    {
+        public string CPRNumber { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string bloodtype { get; set; }
+
+        public string sicknesses { get; set; }
+
+        public string intolerentTo { get; set; }
+        public string mentalDisorders { get; set; }
+        public string remarks { get; set; }
+
+    }
+}

0c34d3abdfacf237616cb5eaab4f4a4a00a5f64d
Yoithebrain <chri86j1@stud.kea.dk>
2018-04-09 09:28:53 +0200

    Window upon edit.

---
+++ Plejehjem Opgave CSharp/Scripts/Textgetter.js
@@ -0,0 +1,12 @@
+﻿//Text getter til pop-up vinduer, i tilfælde af at flere skal bruge den.
+//Made by Christian
+//Used for pop-ups.
+
+
+function clickHanlder() {
+    newWin();
+}
+function newWin() {
+    var mywindow = window.open("", "_blank", "height = 200, width = 300", false);
+    mywindow.focus;
+}

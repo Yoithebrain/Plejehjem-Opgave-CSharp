6d132b6d7a8bdc0f2dda6ae1c52efb87299cbc94
THolm <holmegaard1996@gmail.com>
2018-04-25 15:30:22 +0200

    implemented GeoCode API, saving lat & long when typing in an address. Still need to handle errorhandling, but the basic functionality is there

--- Plejehjem Opgave CSharp/PersonModel.Designer.cs
+++ Plejehjem Opgave CSharp/PersonModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Lamin\source\repos\Plejehjem-Opgave-CSharp\Plejehjem Opgave CSharp\PersonModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\KEA 4 Semester\C#\Projects\HomeCareFinal\Plejehjem-Opgave-CSharp\Plejehjem Opgave CSharp\PersonModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

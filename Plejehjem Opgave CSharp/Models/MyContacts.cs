06d4bb106414cb7ace7a6ac1416a3639edb30c76
Yoithebrain <chri86j1@stud.kea.dk>
2018-05-14 11:26:57 +0200

    Got model and controller to work, now for adding the stuff into the view.

---
+++ Plejehjem Opgave CSharp/Models/MyContacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plejehjem_Opgave_CSharp.Models
+{
+    public class MyContacts
+    {
+        public String _jobtitle { get; set; }
+        public String _firstname { get; set; }
+        public String _lastname { get; set;}
+        public String _phonenumber { get; set; }
+        public String _otherInfo { get; set; }
+        public String _fullName { get; set; }
+
+
+
+    }
+}
